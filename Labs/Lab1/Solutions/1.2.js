3144ee90aa8fa2119cff8545c45b42c59f8be84c
Scripter <jtiks06@gmail.com>
2019-04-08 10:39:08 +0300

    Add test set, fix gen permutations

--- Labs/Lab1/Solutions/1.2.js
+++ Labs/Lab1/Solutions/1.2.js
@@ -1,31 +1,25 @@
 'use strict'
 
 function getAllPermutations(string) {
-    var results = [];
+    let results = [];
   
     if (string.length === 1) {
       results.push(string);
       return results;
     }
   
-    for (var i = 0; i < string.length; i++) {
-      var firstChar = string[i];
-      var charsLeft = string.substring(0, i) + string.substring(i + 1);
-      var innerPermutations = getAllPermutations(charsLeft);
-      for (var j = 0; j < innerPermutations.length; j++) {
+    for (let i = 0; i < string.length; i++) {
+      let firstChar = string[i];
+      let charsLeft = string.substring(0, i) + string.substring(i + 1);
+      let innerPermutations = getAllPermutations(charsLeft);
+      for (let j = 0; j < innerPermutations.length; j++) {
         results.push(firstChar + innerPermutations[j]);
       }
     }
     return results;
   }
 
-function main(){
-
-    var inputString2 = 'ABC'
-    
-    console.log(getAllPermutations(inputString2))
-}
-
-if (require.main === module) {
-    main();
-}
+let testStrings = ['My', 'Bonnie', 'lies', 'over', 'the', 'ocean']
+testStrings.forEach(function(testString){
+  console.log(new Set(getAllPermutations(testString)))
+});
